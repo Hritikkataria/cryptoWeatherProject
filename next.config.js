976df487da542530c544a2ccad359dcb03b9b3a1
Katariahritik <rajputhritik982@gmail.com>
2025-04-06 21:24:03 +0530

    Update configuration for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  basePath: '/cryptoWeatherProject',
+  assetPrefix: '/cryptoWeatherProject/',
   images: {
     domains: ['openweathermap.org'],
+    unoptimized: true
   },
   eslint: {
     ignoreDuringBuilds: true,
